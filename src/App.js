3faac7a9ecc6ea0e5259b52e10b118a1c3d30d12
Stakrotsky <Stakrotsky@gmail.com>
2024-08-25 21:48:01 +0300

    fix 404

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import styles from './app.module.css';
 import { MainPage } from './MainPage';
 import { TaskPage } from './TaskPage';
@@ -12,7 +12,7 @@ export const App = () => {
 					<Route exact path="/" element={<MainPage />} />
 					<Route path="/task/:id" element={<TaskPage />} />
 					<Route path="/404" element={<NotFoundPage />} />
-					<Route path="*" element={<NotFoundPage />} />
+					<Route path="*" element={<Navigate to="/404" />} />
 				</Routes>
 			</div>
 		</Router>
